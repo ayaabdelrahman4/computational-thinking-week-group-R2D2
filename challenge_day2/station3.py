49fd7e15d63668b4feddc5625b0a59099cdcce5e
ayaabdelrahman4 <ayahisham4@gmail.com>
2024-09-03 15:07:15 +0200

    wo

--- challenge_day2/station3.py
+++ challenge_day2/station3.py
@@ -1,7 +1,8 @@
-number = int(input("Type a number?"))
+def solution_station3(n):
+    number = int(input("Type a number?"))
 
-if number % 3 == 0:
-    print(True)
+    if number % 3 == 0:
+        print(True)
 
-else:
-    print(False)
+    else:
+        print(False)
