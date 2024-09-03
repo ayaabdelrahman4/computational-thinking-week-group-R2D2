444ffc853eb300322f95024f84d876040adfe3d5
ayaabdelrahman4 <ayahisham4@gmail.com>
2024-09-03 15:51:28 +0200

    d

--- challenge_day2/solution5.py
+++ challenge_day2/solution5.py
@@ -3,7 +3,7 @@ import pandas as pd
 def solution_station5(n):
     name = input("Enter a name: ")
 
-    df = pd.read_csv("names_list.csv")
+    df = pd.read_csv("/Users/aya/computational-thinking-week-group-R2D2/challenge_day2/names_list.csv")
 
     for index, row in df.iterrows():
         if row["name"] == name:
