f1f5ca28157583a0b5fc4ddec7a95f0bb1c8bc51
GIMCcoding <162916902+GIMCcoding@users.noreply.github.com>
2024-09-03 14:50:56 +0200

    hey

--- challenge_day2/station1.py
+++ challenge_day2/station1.py
@@ -15,5 +15,4 @@ print(solution_station1(0))  # Output: 0
 print(solution_station1(6))  # Output: 8
 print(solution_station1(8))  # Output: 21
 print(solution_station1(28)) # Output: 317811
-print(solution_station1
-      (62)) # Output: 10610209857723
+print(solution_station1(62)) # Output: 10610209857723
